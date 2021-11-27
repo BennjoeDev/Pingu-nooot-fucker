6d09059f32676b3c339dece81ce6ca0494dccbab
BennjoeDev <91271936+BennjoeDev@users.noreply.github.com>
2021-11-27 16:57:55 +0100

    Added the base of the virus
    Added js, html and css to create a base of the virus

---
+++ pingunootfucker/click.js
@@ -0,0 +1,11 @@
+
+
+function inst(){
+    let left;
+    let top;
+    left = 200;
+    top = 200;
+
+    var Window;
+    Window = window.open("index.html", "", 'width=200, height=200 top=200, left=2100');
+}
