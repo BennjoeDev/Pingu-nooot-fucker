059c6aca65d4fcb6ed3c43996cd3fbead5034f54
BennjoeDev <91271936+BennjoeDev@users.noreply.github.com>
2021-11-27 23:10:25 +0100

    Update click.js

--- pingunootfucker/click.js
+++ pingunootfucker/click.js
@@ -1,11 +1,7 @@
 
 
 function inst(){
-    let left;
-    let top;
-    left = 200;
-    top = 200;
-
+    
     var Window;
-    Window = window.open("index.html", "", 'width=200, height=200 top=200, left=2100');
+    Window = window.open("index.html", "", "width=200, height=200 top=200, left=2100");
 }
